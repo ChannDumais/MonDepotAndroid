918a48b495eeef96fd6960d6797be88d645e76f3
ChannDumais <1956964@cegepmontpetit.ca>
2025-10-21 09:44:26 -0400

    FCT : Exercice Album Page détails

--- app/src/main/java/com/example/composelistelazycol/MainActivity.kt
+++ app/src/main/java/com/example/composelistelazycol/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.composelistelazycol
 
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -13,7 +16,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composelistelazycol.ui.theme.ComposeListeLazyColTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,12 +32,17 @@ class MainActivity : ComponentActivity() {
                         .fillMaxSize()
                         .padding(innerPadding)) {
 
-                        val albums = listOf<String>("Antigéographiquement", "J'ai Bu", "Comment Debord",
-                            "Darlène", "La nuit est une Panthère", "Jaune")
+                        val listeAlbums = listOf<Album>(
+                            Album(1, "Antigéographiquement", "Jérome 50"),
+                            Album(2, "J'ai Bu", "Quebec Redneck"),
+                            Album(3, "La nuit est une Panthère", "Comment Debord"),
+                            Album(4, "Jaune", "Jean-Pierre"),
+                            Album(5, "L'Enfant Sauvage", "Gojira"),
+                        )
 
-                        for (chanson in albums){
+                        for (album in listeAlbums){
                             item {
-                                ChansonItem(chanson, modifier = Modifier.padding(15.dp).height(150.dp))
+                                ChansonItem(album, modifier = Modifier.padding(15.dp).height(150.dp))
                             }
                         }
                     }
@@ -42,12 +52,22 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+data class Album(
+    val id: Int,
+    val name: String,
+    val artistName: String,
+)
 
 @Composable
-fun ChansonItem(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = name,
-        modifier = modifier
-    )
+fun ChansonItem(name: Album, modifier: Modifier = Modifier) {
+    Column(modifier) {
+        Text(
+            text = name.name,
+        )
+        Text(
+            text = name.artistName,
+            fontSize = 12.sp
+        )
+    }
 
 }
