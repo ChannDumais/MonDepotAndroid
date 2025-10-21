e3bea07519379808d5611e726fab1ef7753bdc11
ChannDumais <1956964@cegepmontpetit.ca>
2025-10-21 09:07:55 -0400

    FCT : Album Favoris Base

--- app/src/main/java/com/example/composelistelazycol/MainActivity.kt
+++ app/src/main/java/com/example/composelistelazycol/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composelistelazycol.ui.theme.ComposeListeLazyColTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +23,31 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeListeLazyColTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    LazyColumn(modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding)) {
+
+                        val albums = listOf<String>("Antigéographiquement", "J'ai Bu", "Comment Debord",
+                            "Darlène", "La nuit est une Panthère", "Jaune")
+
+                        for (chanson in albums){
+                            item {
+                                ChansonItem(chanson, modifier = Modifier.height(150.dp))
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun ChansonItem(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = name,
         modifier = modifier
     )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeListeLazyColTheme {
-        Greeting("Android")
-    }
 }
